0dcde6ea246022bd071d6c657bb2a3a54887e70d
Ezequiel Gerónimo Gonzalez <ezequiel@adaitw.com.ar>
2018-10-18 15:19:22 -0300

    add new dom manipulation examples

---
+++ dom/style/02_style.js
@@ -0,0 +1,7 @@
+var losPes = document.getElementsByTagName('p')
+
+losPes[0].style.color = 'red'
+losPes[1].innerHTML = 'hola mundo 2!'
+
+document.getElementById('photo').style.backgroundImage = 'url(https://www.hastac.org/sites/default/files/styles/post_image/public/upload/images/opportunity/code.jpg?itok=EmRIJ6NS)'
+
